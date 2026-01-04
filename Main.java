ee5d9ea70119a28b49daff1a1bd4236d338bdcec
Marco Montero Justiniano <marcomonterojustiniano@MacBook-Air-de-Marco.local>
2026-01-04 13:48:38 -0800

    ARRAYS Practice array manipulation with index boundaries

--- Main.java
+++ Main.java
@@ -1,35 +1,29 @@
 import java.util.Scanner;
 
-
     public class Main{
         public static void main(String[] args){
 
             int[] arr = {1,2,3,4,5,6,7,8,9,10};
             int[] arr2 = new int[arr.length];
-            arr2[0] = arr[0];
-            int j = 1;
-            int sum = 0;
+            int k = 5;
 
+            for(int i = 0;i<5;i++){
 
-          
+                arr2[i] = arr[i];
 
-            for(int i = 1;i<arr.length;i++){
-
-                sum += arr[i-1];
-
-                arr2[j]=sum;
-                j++;
             }
 
-
-
-
-
-for (int z : arr2) {
-    System.out.println(z);
-}
-
-
+            for(int i = 9;i>4;i--){
+            
+            arr2[k] = arr[i];
+            k++;
 
         }
+
+        for (int z : arr2) {
+            System.out.println(z);
+        }
+
+    }
+
     }
