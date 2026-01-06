c899e0cc0c8358f9251c11995b5ab7df582de1c1
Marco Montero Justiniano <marcomonterojustiniano@MacBook-Air-de-Marco.local>
2026-01-05 16:15:14 -0800

    ARRAYS Fastest and second runner

--- Main.java
+++ Main.java
@@ -1,58 +1,52 @@
 import java.util.Scanner;
 
+    public class Main {
+        public static void main (String[] arg) {
 
-    public class Main{
-
-        static void Rotate(int[] arr){
-
-            int temp = arr[0];
-
-            for(int i = 1;i<arr.length;i++){
-
-                arr[i-1] = arr[i];
-            }
-            arr[arr.length-1] = temp;
-        }
-
-        static void rotate(int[] arr,int k){
-
-            k %= arr.length;
-            if(k<0){
-
-                k += arr.length;
-
-            }
-
-            for(int i = 1;i<=k;i++){
-
-                Rotate(arr);
-
-            }
-        }
+            String[] names = {
+"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
+"Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
+"Aaron", "Kate"
+};
+                int[] times = {
+341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
+343, 317, 265
+};
 
 
-        static void main(String[] args){
-            Scanner o = new Scanner(System.in);
-            int[] arr = {1,2,3,4,5,6,7,8,9,10};
+int min;
+int min2;
+int minIndex = 0;
+int minIndex2 = 0; 
 
-            System.out.println("Enter a number,positive for turnin right(e.g 2 3 4 5 1 ) or negative to turn left (e.g 5 1 2 3 4)" );
-            int k = o.nextInt();
+    if(times[0]<times[1]){
+        min = times[0];
+        min2 = times[1];
+    }else{
+        min = times[1];
+    min2 = times[0];
+    }
 
- 
-            rotate(arr,k);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+    for(int i = 0;i<times.length;i++){
+
+        if(min>times[i]){
+            min2 = min;
+            min = times[i];
+            minIndex = i;
+
+        }else{
+
+            min2 = times[i];
+            minIndex2 = i;
         }
     }
+
+
+    System.out.println("The fastest runner is: "+names[minIndex]+" and his/her time is: "+times[minIndex] );
+    System.out.println();
+    System.out.println();
+    System.out.println("The second fastest runner is: "+names[minIndex2]+" and his/her time is: "+times[minIndex2]);
+    
+}
+}
