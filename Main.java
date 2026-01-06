8094b7f4cb0fa48681bd698ea607cb4daf953bb6
Marco Montero Justiniano <marcomonterojustiniano@MacBook-Air-de-Marco.local>
2026-01-06 13:57:07 -0800

    ARRAYS First repeated element O(n²)

--- Main.java
+++ Main.java
@@ -1,52 +1,28 @@
 import java.util.Scanner;
 
-    public class Main {
-        public static void main (String[] arg) {
 
-            String[] names = {
-"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
-"Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
-"Aaron", "Kate"
-};
-                int[] times = {
-341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
-343, 317, 265
-};
+    public class Main{
+        public static void main(String[] args){
 
+            int[] arr = {1,2,2,3,4,5,6,7,8,9,10,10,10,10,10};
+                int Index = -1;
 
-int min;
-int min2;
-int minIndex = 0;
-int minIndex2 = 0; 
+            outer:
+            for(int i = 0;i<arr.length;i++){
+                for(int k = 0;k<i;k++){
 
-    if(times[0]<times[1]){
-        min = times[0];
-        min2 = times[1];
-    }else{
-        min = times[1];
-    min2 = times[0];
-    }
+                    if(arr[i]==arr[k]){
+                        Index = i;
+                        break outer;
 
+                    }
+                }
+            }
+            if(Index<0)
+                System.out.println("Ther's not number repeated");
+            else{
 
-    for(int i = 0;i<times.length;i++){
-
-        if(min>times[i]){
-            min2 = min;
-            min = times[i];
-            minIndex = i;
-
-        }else{
-
-            min2 = times[i];
-            minIndex2 = i;
+                System.out.println("The first number repeated is: "+arr[Index]);
+            }
         }
     }
-
-
-    System.out.println("The fastest runner is: "+names[minIndex]+" and his/her time is: "+times[minIndex] );
-    System.out.println();
-    System.out.println();
-    System.out.println("The second fastest runner is: "+names[minIndex2]+" and his/her time is: "+times[minIndex2]);
-    
-}
-}
